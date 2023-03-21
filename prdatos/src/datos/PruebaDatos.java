a32b8ca54aec48d1b2fe25b0da1847cf585e2873
Jaime Nevado Farfán <djamperiio@gmail.com>
2023-03-21 09:55:32 +0100

    Cuarta Práctica

---
+++ prdatos/src/datos/PruebaDatos.java
@@ -0,0 +1,13 @@
+package datos;
+
+public class PruebaDatos {
+
+	public static void main(String[] args) {
+
+		String[] valores = { "5", "9", "Pepe", "10", "Maria", "12", "13", "Paco", "17", "20", "Ana", "25", "Juan",
+				"Lola" };
+		Datos datos = new Datos(valores, 10, 20);
+
+		System.out.println(datos.toString());
+	}
+}
